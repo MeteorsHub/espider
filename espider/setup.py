9af044693da74f6b2357d01d5ef70c934191e0d1
MeteorKepler <JimRanor@outlook.com>
2016-07-31 16:32:05 +0800

    adjust path

---
+++ espider/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='espider',
+      version='0.1.0',
+      description='A simply constructed web crawling and web scraping framework that is easy to install and use',
+      author='MeteorKepler',
+      author_email='JimRanor@outlook.com',
+      packages=['espider', 'espider.tools'],
+    )
