28701083c6881a8f32b87a29c0a647fb81e2e107
MeteorKepler <JimRanor@outlook.com>
2016-08-17 10:56:16 +0800

    change version

--- espider/setup.py
+++ espider/setup.py
@@ -18,7 +18,7 @@ __author__ = 'MeterKepler'
 from distutils.core import setup
 
 setup(name='espider',
-      version='0.1.2',
+      version='0.1.3',
       description='A simply constructed web crawling and web scraping framework that is easy to install and use',
       author='MeteorKepler',
       author_email='JimRanor@outlook.com',
